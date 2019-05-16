b520acdb3c8664bb65ce9a8bb7754991707bd6a7
amarbgbg <50252471+amarMusic@users.noreply.github.com>
2019-05-17 01:55:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,7 +207,7 @@ client.on('message',async message => {
     
 if(message.author.bot) return;
 if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 1000000
+    credits: 100
 };
 
 let userData = credits[message.author.id];
@@ -226,7 +226,7 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 });
 
 client.on('message', async message => {
-    let amount = 250;
+    let amount = 100000000;
     if(message.content.startsWith(prefix + "daily")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
